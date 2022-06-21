e6d0fb4b192f2783f902d6610412926740f65856
all4me7 <m.wrzochol@gmail.com>
2022-06-21 10:34:23 +0200

    fix

--- my_modules/always_on.py
+++ my_modules/always_on.py
@@ -6,8 +6,6 @@ import keyboard
 class DataContainer:
     SLEEP_TIME: int = 60
     SHIFT_BUTTON: str = "shift"
-    LOOP_TIME: float = 0.05
-    SPACE_BUTTON: str = "space"
 
 
 def run() -> None:
@@ -17,12 +15,7 @@ def run() -> None:
             sleep(DataContainer.SLEEP_TIME)
             second_mouse_position: tuple[int, int] = mouse.get_position()
             if first_mouse_position == second_mouse_position:
-                for _ in range(100):
-                    sleep(DataContainer.LOOP_TIME)
-                    if keyboard.is_pressed(hotkey=DataContainer.SPACE_BUTTON):
-                        break
-                    else:
-                        keyboard.press_and_release(DataContainer.SHIFT_BUTTON)
+                keyboard.press_and_release(DataContainer.SHIFT_BUTTON)
             else:
                 pass
     except KeyboardInterrupt:
